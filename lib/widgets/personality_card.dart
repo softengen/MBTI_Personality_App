36dc099976566a402bedf1f166c3a7e17fd84148
imransihab0 <imran.sihab.0@gmail.com>
2024-02-15 02:29:56 +0600

    removing debug banner and changing personality_card.dart style

--- lib/widgets/personality_card.dart
+++ lib/widgets/personality_card.dart
@@ -1,44 +1,48 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class personality_card extends StatelessWidget{
   final List body;
   final VoidCallback callBack;
-  personality_card({required this.body, required this.callBack});
+  const personality_card({required this.body, required this.callBack});
 
   @override
   Widget build(BuildContext context) {
     print(body);
+
+    // these two line for accessing screen real size
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+
     return Material(
       child: InkWell(
         onTap: callBack,
         child: Container(
-          margin: EdgeInsets.all(11),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            boxShadow: [BoxShadow(blurRadius: 10,spreadRadius: 5)],
-            color: Colors.deepPurple.shade700,
+          margin: const EdgeInsets.all(7),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(9)),
+            // boxShadow: [BoxShadow(blurRadius: 0,spreadRadius: 0)],
+            color: Color(0xFF258654),
           ),
           width: 380,
           height: 300,
           child: Column(
             children: [
-              SizedBox(height: 40,),
+              const SizedBox(height: 40,),
               Container(
-                margin: EdgeInsets.all(10),
+                margin: const EdgeInsets.all(10),
                 height: 60,
                 child: Text(body[1],
-                  style: TextStyle(fontFamily: "Open Shadow", fontSize: 45,color: Colors.white),),
+                  style: const TextStyle(fontFamily: "Open Sans", fontSize: 45,color: Colors.white),),
               ),
               Padding(
                 padding: const EdgeInsets.only(bottom: 20),
-                child: Text(("(${body[0]})"), style: TextStyle(fontFamily: "Cameo Antique", fontSize: 30, color: Colors.white),),
+                child: Text(("(${body[0]})"), style: const TextStyle(fontFamily: "Cameo Antique", fontSize: 30, color: Colors.white),),
               ),
-              SizedBox(height: 25,),
+              const SizedBox(height: 25,),
               Padding(
                 padding: const EdgeInsets.only(left: 20, right: 20,),
                 child: Text(body[2],
-                  style: TextStyle(fontFamily: "Take Coffee", fontSize: 16, color: Colors.white54),
+                  style: const TextStyle(fontFamily: "Take Coffee", fontSize: 16, color: Colors.white54),
                   textAlign: TextAlign.center,),
               )
             ],
