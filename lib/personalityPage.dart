245629f0c481afe72cfd0b45f352014992982e89
imransihab0 <imran.sihab.0@gmail.com>
2024-02-19 02:01:40 +0600

    updating login_screen.dart style, adding gestureDetector to hide keyboard, and adding scrolling effect on the login page

--- lib/personalityPage.dart
+++ lib/personalityPage.dart
@@ -1,28 +1,91 @@
 import 'package:mbti_app/NavBar.dart';
 import 'package:mbti_app/widgets/personality_card.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'All_Personalities.dart';
 
-class personalityPage extends StatelessWidget{
-  final Map body ={
-    "INTJ" : ["Mastermind","Strategic and independent thinkers, they excel in long-term planning and are driven by their innovative ideas.",INTJ()],
-    "INTP" : ["Logician","Analytical and inventive, they love exploring new ideas and approaches, often seeking knowledge for its own sake.",INTP()],
-    "ENTJ" : ["Commander","Strategic and assertive, they excel in leadership roles, driving for efficiency and achievement.",ENTJ()],
-    "ENTP" : ["Debater","Innovative and curious, they enjoy exploring new concepts and challenging traditional ideas.",ENTP()],
-    "INFJ" : ["Advocate","Insightful and idealistic, they seek deep connections, often advocating for positive change with empathy.",INFJ()],
-    "INFP" : ["Healer","Creative and empathetic, they are driven by their values and aspire to make a positive impact on the world.",INFP()],
-    "ENFJ" : ["Protagonist","Charismatic and compassionate leaders, they inspire and motivate others towards a common goal.",ENFJ()],
-    "ENFP" : ["Visionary","Enthusiastic and imaginative, they inspire others with their creativity and passion for possibilities.",ENFP()],
-    "ISTJ" : ["Inspector","Detail-oriented and organized, they thrive in structured environments and are reliable and responsible.",ISTJ()],
-    "ISFJ" : ["Protector","Compassionate and loyal, they are dedicated caretakers who value tradition and harmony in relationships.",ISFJ()],
-    "ESTJ" : ["Executive","Efficient and organized leaders, they excel in managing tasks and people with a focus on practical outcomes.",ESTJ()],
-    "ESFJ" : ["Entertainer","Supportive and sociable, they thrive on nurturing relationships and creating a harmonious environment.",ESFJ()],
-    "ISTP" : ["Craftsman","Adaptable and hands-on, they enjoy solving practical problems and excel in troubleshooting.",ISTP()],
-    "ISFP" : ["Artist","Artistic and sensitive, they have a strong appreciation for aesthetics and value individual expression.",ISFP()],
-    "ESTP" : ["Dynamo","Energetic and spontaneous, they thrive in dynamic environments and enjoy taking risks for excitement.",ESTP()],
-    "ESFP" : ["Performer","Outgoing and playful, they are natural entertainers who bring energy and enthusiasm to social settings.",ESFP()],
+class personalityPage extends StatelessWidget {
+  final Map body = {
+    "INTJ": [
+      "Mastermind",
+      "Strategic and independent thinkers, they excel in long-term planning and are driven by their innovative ideas.",
+      INTJ()
+    ],
+    "INTP": [
+      "Logician",
+      "Analytical and inventive, they love exploring new ideas and approaches, often seeking knowledge for its own sake.",
+      INTP()
+    ],
+    "ENTJ": [
+      "Commander",
+      "Strategic and assertive, they excel in leadership roles, driving for efficiency and achievement.",
+      ENTJ()
+    ],
+    "ENTP": [
+      "Debater",
+      "Innovative and curious, they enjoy exploring new concepts and challenging traditional ideas.",
+      ENTP()
+    ],
+    "INFJ": [
+      "Advocate",
+      "Insightful and idealistic, they seek deep connections, often advocating for positive change with empathy.",
+      INFJ()
+    ],
+    "INFP": [
+      "Healer",
+      "Creative and empathetic, they are driven by their values and aspire to make a positive impact on the world.",
+      INFP()
+    ],
+    "ENFJ": [
+      "Protagonist",
+      "Charismatic and compassionate leaders, they inspire and motivate others towards a common goal.",
+      ENFJ()
+    ],
+    "ENFP": [
+      "Visionary",
+      "Enthusiastic and imaginative, they inspire others with their creativity and passion for possibilities.",
+      ENFP()
+    ],
+    "ISTJ": [
+      "Inspector",
+      "Detail-oriented and organized, they thrive in structured environments and are reliable and responsible.",
+      ISTJ()
+    ],
+    "ISFJ": [
+      "Protector",
+      "Compassionate and loyal, they are dedicated caretakers who value tradition and harmony in relationships.",
+      ISFJ()
+    ],
+    "ESTJ": [
+      "Executive",
+      "Efficient and organized leaders, they excel in managing tasks and people with a focus on practical outcomes.",
+      ESTJ()
+    ],
+    "ESFJ": [
+      "Entertainer",
+      "Supportive and sociable, they thrive on nurturing relationships and creating a harmonious environment.",
+      ESFJ()
+    ],
+    "ISTP": [
+      "Craftsman",
+      "Adaptable and hands-on, they enjoy solving practical problems and excel in troubleshooting.",
+      ISTP()
+    ],
+    "ISFP": [
+      "Artist",
+      "Artistic and sensitive, they have a strong appreciation for aesthetics and value individual expression.",
+      ISFP()
+    ],
+    "ESTP": [
+      "Dynamo",
+      "Energetic and spontaneous, they thrive in dynamic environments and enjoy taking risks for excitement.",
+      ESTP()
+    ],
+    "ESFP": [
+      "Performer",
+      "Outgoing and playful, they are natural entertainers who bring energy and enthusiasm to social settings.",
+      ESFP()
+    ],
   };
 
   final List<Hero> widgets = [];
@@ -31,25 +94,33 @@ class personalityPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     body.forEach((key, value) {
-      widgets.add(Hero(
-        tag: key,
-        child: Center(
-          child: personality_card(
-            body: [key,value[0],value[1]],
-            callBack: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => value[2]));
-            },
+      widgets.add(
+        Hero(
+          tag: key,
+          child: Center(
+            child: personality_card(
+              body: [key, value[0], value[1]],
+              callBack: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => value[2]));
+              },
+            ),
           ),
         ),
-      ),);
+      );
     });
 
     return Scaffold(
       drawer: NavBar(),
       appBar: AppBar(
-        title: Center(child: Text("Personalities",
-          style: Theme.of(context).textTheme.displayLarge!.copyWith(fontSize: 30, color:
-        Colors.white),)),
+        title: Center(
+            child: Text(
+          "Personalities",
+          style: Theme.of(context)
+              .textTheme
+              .displayLarge!
+              .copyWith(fontSize: 30, color: Colors.white),
+        )),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -58,5 +129,4 @@ class personalityPage extends StatelessWidget{
       ),
     );
   }
-
 }
