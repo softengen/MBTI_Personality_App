b36870fcc11a9c302dc201758a6e5acdf405b236
imransihab0 <imran.sihab.0@gmail.com>
2024-02-18 20:03:15 +0600

    updating login_screen.dart style, adding gestureDetector to hide keyboard, and adding scrolling effect on the login page

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -23,88 +23,84 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // title: const Text(
-        //   'Login Page',
-        //   style: TextStyle(
-        //     fontSize: 28,
-        //     fontWeight: FontWeight.bold,
-        //   ),
-        // ),
-        // centerTitle: true,
-        // titleSpacing: -10,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const FlutterLogo(size: 100),
-            
-            const SizedBox(height: 60),
-            const SizedBox(height: 40),
-
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                labelStyle: TextStyle(
-                  fontFamily: 'UbuntuMono',
-                  fontWeight: FontWeight.bold
-                ),
-                icon: Icon(Icons.person),
-
-              ),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                labelStyle: TextStyle(
-                  fontFamily: 'UbuntuMono',
-                  fontWeight: FontWeight.bold,
-                ),
-                icon: Icon(Icons.lock),
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 40),
-            Container(
-              height: 60,
-              width: 190,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Add your login logic here
-                  String username = _usernameController.text;
-                  String password = _passwordController.text;
-
-                  // here authentication logic will be added later
-                  // For now, let's just print the username and password on the console
-
-                  log('Username: $username');
-                  log('Password: $password');
-                },
-                child: const Text(
-                    'LOGIN',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'UbuntuMono',
+      appBar: AppBar(),
+      body: GestureDetector(
+        onTap: () {
+          // Hide the keyboard when the screen is tapped
+          FocusScope.of(context).unfocus();
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const FlutterLogo(size: 100),
+                const SizedBox(height: 60),
+                const SizedBox(height: 40),
+                TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Username',
+                    labelStyle: TextStyle(
+                      fontFamily: 'UbuntuMono',
+                      fontWeight: FontWeight.bold,
+                    ),
+                    icon: Icon(Icons.person),
                   ),
                 ),
-                // design of elevated button
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white,
-                  onPrimary: Colors.deepPurple,
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6)),
-                  // padding: EdgeInsets.all(1),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(
+                      fontFamily: 'UbuntuMono',
+                      fontWeight: FontWeight.bold,
+                    ),
+                    icon: Icon(Icons.lock),
+                  ),
+                  obscureText: true,
                 ),
-              ),
+                const SizedBox(height: 40),
+                Container(
+                  height: 60,
+                  width: 190,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Add your login logic here
+                      String username = _usernameController.text;
+                      String password = _passwordController.text;
+
+                      // here authentication logic will be added later
+                      // For now, let's just print the username and password on the console
+
+                      log('Username: $username');
+                      log('Password: $password');
+                    },
+                    child: const Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'UbuntuMono',
+                      ),
+                    ),
+                    // design of elevated button
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      onPrimary: Colors.deepPurple,
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6)),
+                      // padding: EdgeInsets.all(1),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
