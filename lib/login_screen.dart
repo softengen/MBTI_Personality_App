0731ec9609ac0e4ab2d72fa4e533c2d00a5c6e9f
imransihab0 <imran.sihab.0@gmail.com>
2024-04-20 18:54:29 +0600

    updating willPop

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mbti_app/NavBar.dart';
 import 'package:mbti_app/personalityPage.dart';
 import 'package:mbti_app/signup.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: LoginPage(),
     );
-  }
+  } 
 }
 
 class LoginPage extends StatefulWidget {
@@ -44,106 +45,114 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurple[100],
-      body: GestureDetector(
-        onTap: () {
-          // Hide the keyboard when the screen is tapped
-          FocusScope.of(context).unfocus();
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 120),
-                const FlutterLogo(size: 100),
-                const SizedBox(height: 60),
-                const SizedBox(height: 40),
-                TextField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    labelStyle: TextStyle(
-                      fontFamily: '',
-                      fontWeight: FontWeight.bold,
-                    ),
-                    icon: Icon(Icons.person),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  controller: _passController,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(
-                      fontFamily: '',
-                      fontWeight: FontWeight.bold,
-                    ),
-                    icon: Icon(Icons.lock),
-                  ),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 40),
+    return WillPopScope(
+      onWillPop: () async {
+        // exit the app
+        SystemNavigator.pop();
 
-                // < -- Log in button section -- >
-
-                Container(
-                  height: 60,
-                  width: 160,
-                  child: ElevatedButton(
-                    onPressed: _logIn,
-                    // design of elevated button
-                    style: ElevatedButton.styleFrom(
-                      // primary: Colors.white,
-                      // onPrimary: Colors.deepPurple,
-                      // backgroundColor: const Color(0x001d3c45),
-                      backgroundColor: const Color(0x001d3c45),
-                      elevation: 0.7,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6)),
-                      // padding: EdgeInsets.all(1),
-                    ),
-                    child: const Text(
-                      'LOGIN',
-                      style: TextStyle(
-                        fontSize: 19,
-                        fontWeight: FontWeight.bold,
+        return false;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.deepPurple[100],
+        body: GestureDetector(
+          onTap: () {
+            // Hide the keyboard when the screen is tapped
+            FocusScope.of(context).unfocus();
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 120),
+                  const FlutterLogo(size: 100),
+                  const SizedBox(height: 60),
+                  const SizedBox(height: 40),
+                  TextField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
                         fontFamily: '',
-                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      icon: Icon(Icons.person),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: _passController,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                        fontFamily: '',
+                        fontWeight: FontWeight.bold,
+                      ),
+                      icon: Icon(Icons.lock),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 40),
+      
+                  // < -- Log in button section -- >
+      
+                  Container(
+                    height: 60,
+                    width: 160,
+                    child: ElevatedButton(
+                      onPressed: _logIn,
+                      // design of elevated button
+                      style: ElevatedButton.styleFrom(
+                        // primary: Colors.white,
+                        // onPrimary: Colors.deepPurple,
+                        // backgroundColor: const Color(0x001d3c45),
+                        backgroundColor: const Color(0x001d3c45),
+                        elevation: 0.7,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6)),
+                        // padding: EdgeInsets.all(1),
+                      ),
+                      child: const Text(
+                        'LOGIN',
+                        style: TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: '',
+                          color: Colors.black,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 20),
-
-                // < -- sign up button section -- >
-
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const SignUp(),
-                              ));
-                        },
-                        child: const Text(
-                          'Don\'t have an account? Sign Up',
-                          style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            fontSize: 20,
-                            color: Colors.black,
-                          ),
-                        ))
-                  ],
-                )
-              ],
+                  const SizedBox(height: 20),
+      
+                  // < -- sign up button section -- >
+      
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SignUp(),
+                                ));
+                          },
+                          child: const Text(
+                            'Don\'t have an account? Sign Up',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                          ))
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
